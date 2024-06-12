f086b61f606e9687082fd5343f533c31b91cb4da
Simon Forsberg <zomis2k@hotmail.com>
2024-06-13 00:11:20 +0200

    Try a new approach for browser saving

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 import kotlinx.browser.localStorage
 import kotlinx.browser.window
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import net.zomis.motivation.MotivationTask
@@ -12,16 +15,25 @@ import net.zomis.motivation.TasksViewModel
 import org.w3c.dom.get
 import org.w3c.dom.set
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, FlowPreview::class)
 fun main() {
     ComposeViewport(document.body!!) {
         val viewModel = remember { TasksViewModel() }
-        window.onclose = {
-            localStorage["tasks"] = viewModel.serialize()
-        }
         LaunchedEffect(Unit) {
             val data = localStorage["tasks"] ?: return@LaunchedEffect
             viewModel.read(data)
+
+            // Periodically save
+            val flow: Flow<List<MotivationTask>> = flow {
+                while (true) {
+                    emit(viewModel.serializedTasks())
+                    delay(2000)
+                }
+            }
+            // Ignore first - no need to save what we just read
+            flow.drop(1).distinctUntilChanged().debounce(1000).collect {
+                localStorage["tasks"] = viewModel.serialize()
+            }
         }
         App(viewModel)
     }
