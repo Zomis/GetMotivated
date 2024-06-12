ae3fbe6d311e62ff4ded5712075a8e150d01242f
Simon Forsberg <zomis2k@hotmail.com>
2024-06-13 00:11:34 +0200

    Refactor colors

--- composeApp/src/commonMain/kotlin/net/zomis/motivation/Colors.kt
+++ composeApp/src/commonMain/kotlin/net/zomis/motivation/Colors.kt
@@ -2,40 +2,33 @@ package net.zomis.motivation
 
 import androidx.compose.ui.graphics.Color
 
+data class ColorWithBorder(val color: Color, val border: Color)
+data class ColorTheme(val subtask: Color, val task: ColorWithBorder, val group: ColorWithBorder)
+
 class Colors {
 
-    val expectancy = listOf(
-        Color(248, 241, 227),
-        Color(242, 228, 160),
-        Color(239, 208, 84),
-        Color(239, 208, 84),
-        Color(203, 180, 29),
+    val expectancy = ColorTheme(
+        subtask = Color(248, 241, 227),
+        task = ColorWithBorder(color = Color(242, 228, 160), border = Color(239, 208, 84)),
+        group = ColorWithBorder(color = Color(239, 208, 84), border = Color(203, 180, 29)),
     )
 
-    val value = listOf(
-        Color(234, 243, 247),
-        Color(205, 232, 250),
-        Color(172, 212, 239),
-        Color(152, 198, 229),
-        Color(102, 163, 204),
+    val value = ColorTheme(
+        subtask = Color(234, 243, 247),
+        task = ColorWithBorder(Color(205, 232, 250), Color(172, 212, 239)),
+        group = ColorWithBorder(Color(152, 198, 229), Color(102, 163, 204)),
     )
 
-    val impulsiveness = listOf(
-        Color(230, 236, 219),
-        Color(210, 242, 160),
-        Color(195, 224, 150),
-        Color(173, 215, 106),
-        Color(126, 172, 52),
+    val impulsiveness = ColorTheme(
+        subtask = Color(230, 236, 219),
+        task = ColorWithBorder(Color(210, 242, 160), Color(195, 224, 150)),
+        group = ColorWithBorder(Color(173, 215, 106), Color(126, 172, 52)),
     )
 
-    val red = listOf(
-        Color(252, 240, 235),
-        Color(250, 217, 205),
-        Color(250, 198, 180),
-        Color(245, 172, 147),
-        Color(229, 153, 123),
+    val red = ColorTheme(
+        subtask = Color(252, 240, 235),
+        task = ColorWithBorder(Color(250, 217, 205), Color(250, 198, 180)),
+        group = ColorWithBorder(Color(245, 172, 147), Color(229, 153, 123)),
     )
-
-    // every color list is: light, darker, darker border, even darker, even darker border
 
 }
