5a748a90d8e47fa0f28e11a0d71fe7cc321a096d
Simon Forsberg <zomis2k@hotmail.com>
2024-06-12 23:00:09 +0200

    Add list of colors - not yet used

---
+++ composeApp/src/commonMain/kotlin/net/zomis/motivation/Colors.kt
@@ -0,0 +1,41 @@
+package net.zomis.motivation
+
+import androidx.compose.ui.graphics.Color
+
+class Colors {
+
+    val expectancy = listOf(
+        Color(248, 241, 227),
+        Color(242, 228, 160),
+        Color(239, 208, 84),
+        Color(239, 208, 84),
+        Color(203, 180, 29),
+    )
+
+    val value = listOf(
+        Color(234, 243, 247),
+        Color(205, 232, 250),
+        Color(172, 212, 239),
+        Color(152, 198, 229),
+        Color(102, 163, 204),
+    )
+
+    val impulsiveness = listOf(
+        Color(230, 236, 219),
+        Color(210, 242, 160),
+        Color(195, 224, 150),
+        Color(173, 215, 106),
+        Color(126, 172, 52),
+    )
+
+    val red = listOf(
+        Color(252, 240, 235),
+        Color(250, 217, 205),
+        Color(250, 198, 180),
+        Color(245, 172, 147),
+        Color(229, 153, 123),
+    )
+
+    // every color list is: light, darker, darker border, even darker, even darker border
+
+}
